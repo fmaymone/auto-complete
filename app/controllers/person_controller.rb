c27ab6a6fdc0ef58a1503b759c9015f7693d7d52
Fernando Maymone <fmaymone@gmail.com>
2022-12-27 09:37:19 -0300

    ADD new ordered system to endpoints

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class PersonController < ApplicationController
+  def index
+    @result = Person.order(times: :desc).order(:name).limit(ENV['SUGGESTION_NUMBER'])
+    render json: @result
+  end
+
   def search
-    q = params[:string_to_search].downcase
-    @result = Person.where("name LIKE '%#{q}%'").order(:times).limit(ENV['SUGGESTION_NUMBER'])
+    q = params[:prefix].downcase
+    @result = Person.where("name LIKE '%#{q}%'").order(times: :desc).order(:name).limit(ENV['SUGGESTION_NUMBER'])
     render json: @result
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'typehead/:string_to_search' => 'person#search'
+  get 'typehead/:prefix' => 'person#search'
+  get 'typehead' => 'person#index'
 end
-
 
 # Rails.application.routes.draw do
 #   namespace :api do
 #     namespace :v1 do
 #       resources :posts
 #     end
-#   end 
+#   end
 # end
